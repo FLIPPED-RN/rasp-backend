cb06a6c565a2c9e38dc68c12e10df44f66a130af
Ruslan Nazarov <rnazarov142@gmail.com>
2024-04-28 18:35:53 +0200

    backend rasp update

--- server.js
+++ server.js
@@ -1,46 +1,26 @@
 const express = require('express');
 const fs = require('fs');
-const next = require('next');
-const path = require('path');
+const app = express();
+const cors = require('cors'); // Подключаем модуль CORS
 
-const dev = process.env.NODE_ENV !== 'production';
-const appNext = next({ dev, dir: path.resolve(__dirname, 'rasp-frontend') });
-const handle = appNext.getRequestHandler();
+app.use(express.json());
+app.use(cors()); // Используем CORS middleware для разрешения запросов от другого источника
 
-appNext.prepare().then(() => {
-    const app = express();
-    const port = process.env.PORT || 3000;
-
-    app.use(express.json());
-
-    // API-маршрут для поиска
-    app.get('/api/search', (req, res) => {
-        const groupName = req.query.group;
-        fs.readFile('rasp-data.json', 'utf8', (err, data) => {
-            if (err) {
-                console.error(err);
-                return res.status(500).send('Ошибка чтения файла');
-            }
-            const groups = JSON.parse(data);
-            if (groups[groupName]) {
-                res.json(groups[groupName]);
-            } else {
-                res.status(404).send('Группа не найдена');
-            }
-        });
+app.get('/api/search', (req, res) => {
+    const groupName = req.query.group;
+    fs.readFile('rasp-data.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Ошибка чтения файла');
+        }
+        const groups = JSON.parse(data);
+        if (groups[groupName]) {
+            res.json(groups[groupName]);
+        } else {
+            res.status(404).send('Группа не найдена');
+        }
     });
-
-    // Маршрут для обслуживания Next.js приложения
-    app.get('*', (req, res) => {
-        return handle(req, res);
-    });
-
-    // Запуск Express.js сервера
-    app.listen(port, (err) => {
-        if (err) throw err;
-        console.log(`Сервер запущен на порту ${port}`);
-    });
-}).catch((ex) => {
-    console.error(ex.stack);
-    process.exit(1);
 });
+
+const port = process.env.PORT || 4200;
+app.listen(port, () => console.log(`Сервер запущен на порту ${port}`));
