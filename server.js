a3b9ae1092db2e3bc03435ef71928287a56f6309
Ruslan Nazarov <rnazarov142@gmail.com>
2024-04-28 17:46:52 +0200

    backend rasp update

--- server.js
+++ server.js
@@ -1,35 +1,26 @@
 const express = require('express');
-const next = require('next');
-const dev = process.env.NODE_ENV !== 'production';
-const nextApp = next({ start, dir: '../rasp-frontend' });
-const handle = nextApp.getRequestHandler();
 const fs = require('fs');
+const app = express();
 const cors = require('cors'); // Подключаем модуль CORS
 
-nextApp.prepare().then(() => {
-  const app = express();
+app.use(express.json());
+app.use(cors()); // Используем CORS middleware для разрешения запросов от другого источника
 
-  app.use(express.json());
-  app.use(cors()); // Используем CORS middleware для разрешения запросов от другого источника
-
-  app.get('/api/search', (req, res) => {
+app.get('/api/search', (req, res) => {
     const groupName = req.query.group;
     fs.readFile('rasp-data.json', 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).send('Ошибка чтения файла');
-      }
-      const groups = JSON.parse(data);
-      if (groups[groupName]) {
-        res.json(groups[groupName]);
-      } else {
-        res.status(404).send('Группа не найдена');
-      }
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Ошибка чтения файла');
+        }
+        const groups = JSON.parse(data);
+        if (groups[groupName]) {
+            res.json(groups[groupName]);
+        } else {
+            res.status(404).send('Группа не найдена');
+        }
     });
-  });
-
-  app.all('*', (req, res) => handle(req, res));
-
-  const port = process.env.PORT || 4200;
-  app.listen(port, () => console.log(`Сервер запущен на порту ${port}`));
 });
+
+const port = process.env.PORT || 4200;
+app.listen(port, () => console.log(`Сервер запущен на порту ${port}`));
